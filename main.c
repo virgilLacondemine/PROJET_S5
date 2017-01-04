c71de3663da9a8dc62619533044f242e4af556e3
Mo4z <that-mo4z@outlook.fr>
2017-01-04 15:39:35 +0100

    modifications mineures du main.c

--- main.c
+++ main.c
@@ -6,27 +6,33 @@
 
 int main(int argc, char const *argv[]) {
 
+	// On ouvre le premier fichier
+	FILE* f1;
+	f1 = fopen(argv[2], "r");
+
+	// On definie et alloue la memoire pour
+	// chaque partie du fichier ELF
+	Elf32_Ehdr *elfHeader;
+	elfHeader = malloc(sizeof * elfHeader);
+
+	// On stocke chaque partie du fichier ELF
+	fseek(f1, 0, SEEK_SET);
+	fread(elfHeader, sizeof(Elf32_Ehdr), 1, f1);
+
+	// Si le fichier n'est pas au format ELF, il y a une erreur
+	if ( !check_header(elfHeader) )
+	{
+		return -1;
+	}
+
+	// Affichage de l'en-tête si option "-h"
 	if ( !strcmp(argv[1], "-h") )
 	{
-		Elf32_Ehdr *elfHeader;
-		FILE* f1;
-
-		elfHeader = malloc(sizeof * elfHeader);
-			 	
-		f1 = fopen(argv[2], "r");
-
-		fseek(f1, 0, SEEK_SET);
-
-		fread(elfHeader, sizeof(Elf32_Ehdr), 1, f1);
-
-		if ( check_header(elfHeader) )
-		{
-			read_elf_header(elfHeader);
-		}
-		free(elfHeader);
-		fclose(f1);
+		read_elf_header(elfHeader);
 	}
-	
+
+	// si aucune des option valable n'est utilisee en
+	// second argument, on affiche l'ensemble des options 
 	else
 	{
 		printf("	-h : Affichage de l'en-tête\n");
@@ -35,6 +41,10 @@ int main(int argc, char const *argv[]) {
 		printf("	-s : Affichage de la table des symboles\n");
 		printf("	-r : Affichage des table de reimplantation\n");
 	}
+
+	// On libere toutes la memoire allouee precedement
+	free(elfHeader);
+	fclose(f1);
 
 	return 0;
 }
